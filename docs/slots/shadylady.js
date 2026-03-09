729c8029bdd69f7a171b96dfa6758b9270b1ce87
_ugabuga <ugabugabusiness@gmail.com>
2026-03-09 18:50:20 +0200

    v1.6.03

---
+++ docs/slots/shadylady.js
@@ -0,0 +1,62 @@
+SLOT_DATABASE.push(
+  {
+    name: "Suck",
+    provider: "Shady Lady",
+    maxWin: "20000x",
+    demo: "https://clashofslots.com/slots/shady-lady/suck/",
+  },
+  {
+    name: "Laced",
+    provider: "Shady Lady",
+    maxWin: "20000x",
+    demo: "https://clashofslots.com/slots/shady-lady/laced/",
+  },
+  {
+    name: "Black Friday",
+    provider: "Shady Lady",
+    maxWin: "20000x",
+    demo: "https://clashofslots.com/slots/shady-lady/black-friday/",
+  },
+  {
+    name: "Sew",
+    provider: "Shady Lady",
+    maxWin: "20000x",
+    demo: "https://clashofslots.com/slots/shady-lady/sew/",
+  },
+  {
+    name: "Swoll",
+    provider: "Shady Lady",
+    maxWin: "20000x",
+    demo: "https://clashofslots.com/slots/shady-lady/swoll/",
+  },
+  {
+    name: "Orphan Organ",
+    provider: "Shady Lady",
+    maxWin: "20000x",
+    demo: "https://clashofslots.com/slots/shady-lady/orphan-organ/",
+  },
+  {
+    name: "Office Party",
+    provider: "Shady Lady",
+    maxWin: "20000x",
+    demo: "https://clashofslots.com/slots/shady-lady/office-party/",
+  },
+  {
+    name: "Oops",
+    provider: "Shady Lady",
+    maxWin: "20000x",
+    demo: "https://clashofslots.com/slots/shady-lady/oops/",
+  },
+  {
+    name: "Brainwashed",
+    provider: "Shady Lady",
+    maxWin: "20000x",
+    demo: "https://clashofslots.com/slots/shady-lady/brainwashed/",
+  },
+  {
+    name: "Devil's Finger",
+    provider: "Shady Lady",
+    maxWin: "20000x",
+    demo: "https://clashofslots.com/slots/shady-lady/devils-finger/",
+  },
+);
